f3ad6af80b343ecd3555c748b8b2d12719e1b716
Candy_Tsai <candy02058912@gmail.com>
2018-08-19 05:12:21 +0800

    retrieive bike status information

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-let map;
+let map, largeInfoWindow;
 
-function populateInfoWindow(marker, infoWindow) {
+/**
+ * Shows the info window when marker is clicked
+ *
+ * @param {*} marker - the marker clicked
+ * @param {*} infoWindow - window that popups on top of the marker
+ * @param {*} api - check if bikes are available api
+ * @param {*} stationUid - uid of the bike station used to filter api
+ */
+function populateInfoWindow(marker, infoWindow, api, stationUid) {
   if (infoWindow.marker != marker) {
-    infoWindow.marker = marker;
-    infoWindow.setContent(marker.title);
-    infoWindow.open(map, marker);
-  }
+    $.getJSON(`${api}&$filter eq '${stationUid}'`).done(([status]) => {
+      infoWindow.marker = marker;
+        infoWindow.setContent(`<div>
+        <h4>${marker.title}</h4>
+        <p>Service <span class=${status.ServieAvailable === 1 ? 'text-success>Available' : 'text-danger>Unavailable'}</span></p>
+        <p>Bikes for rent: ${status.AvailableRentBikes}</p>
+        <p>Space for return: ${status.AvailableReturnBikes}</p> 
+        </div>`);
+        infoWindow.open(map, marker);
+    }).failed(err => alert('Could not retrieve information.'));
+    }
 };
+
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
@@ -14,7 +30,7 @@ function initMap() {
     center: {lat: 24.9875278, lng: 121.3645979},
     zoom: 11
   });
-  largeInfowindow = new google.maps.InfoWindow();
+  largeInfoWindow = new google.maps.InfoWindow();
 
 }
 
@@ -22,6 +38,7 @@ const City = function(data) {
   const self = this;
   this.name = data.name;
   this.api = data.api;
+  this.availabilityApi = data.availabilityApi;
   this.location = data.location;
   this.stations = ko.observableArray();
   this.markers = ko.observableArray();
@@ -35,31 +52,31 @@ const City = function(data) {
       });
       marker.setVisible(false);
       marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
+        populateInfoWindow(this, largeInfoWindow, self.availabilityApi, station.StationUID);
       });
       self.markers.push(marker);
     })
-  });
+  }).fail(err => alert('Could not retrive information'));
 };
 
 const ViewModel = function() {
   const self = this;
-
+  // Search query
   this.query = ko.observable('');
-
+  // Generate cities
   this.cities = ko.observableArray();
   CITIES.forEach(city => {
     self.cities.push(new City(city));
   });
-
+  // Current city user is viewing
   this.currentCity = ko.observable(this.cities()[0]);
-
+  // Set city to user's selection
   this.setCity = function(clickedCity) {
     // Remove old city markers
     const prevMarkers = self.currentCity().markers();
     prevMarkers.forEach(marker => marker.setVisible(false));
     // Close info window
-    largeInfowindow.close();
+    largeInfoWindow.close();
     // Set current city to the clicked city
     self.currentCity(clickedCity);
     self.query('');
@@ -72,10 +89,11 @@ const ViewModel = function() {
       const isMatched = station.StationName.En.toLowerCase().includes(self.query().toLowerCase());
       if (markers[idx]) { markers[idx].setVisible(isMatched); }
       if(isMatched) { return true; }
+      largeInfoWindow.close();
     });
     return results;
   }, this);
-
+  // Show info window when click on station name in sidebar
   this.clickStation = function(clickedStation) {
     const stations = self.currentCity().stations();
     const markers = self.currentCity().markers();
--- js/constants.js
+++ js/constants.js
@@ -7,11 +7,14 @@ const CITIES = [
   {
     name: 'Taipei City',
     location: {lat: 24.9875278, lng: 121.3645979},
-    api: 'https://ptx.transportdata.tw/MOTC/v2/Bike/Station/Taipei?$format=JSON'
+    api: 'https://ptx.transportdata.tw/MOTC/v2/Bike/Station/Taipei?$format=JSON',
+    availabilityApi: 'https://ptx.transportdata.tw/MOTC/v2/Bike/Availability/Taipei?$format=JSON',
   },
   {
     name: 'Taoyuan City',
+    id: 'Taoyuan',
     location: {lat: 24.9875278, lng: 121.3645979},
-    api: 'https://ptx.transportdata.tw/MOTC/v2/Bike/Station/Taoyuan?$format=JSON'
+    api: 'https://ptx.transportdata.tw/MOTC/v2/Bike/Station/Taoyuan?$format=JSON',
+    availabilityApi: 'https://ptx.transportdata.tw/MOTC/v2/Bike/Availability/Taoyuan?$format=JSON'
   },
 ];
